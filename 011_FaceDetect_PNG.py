e1a5633f1184e89216c73c618ecdaed8aa5543f2
Kinda <lamkinun@gmail.com>
2019-12-26 18:51:56 +0800

    bounding check

--- 011_FaceDetect_PNG.py
+++ 011_FaceDetect_PNG.py
@@ -1,7 +1,8 @@
 import cv2
 
 
-cascade = cv2.CascadeClassifier("./haarcascades/haarcascade_frontalface_default.xml")
+path = cv2.haarcascades + "haarcascade_frontalface_default.xml"
+cascade = cv2.CascadeClassifier(path)
 png = cv2.imread("./images/sample_03.png", cv2.IMREAD_UNCHANGED)
 
 camera = cv2.VideoCapture(0)
@@ -19,10 +20,14 @@ while camera.isOpened():
         h2 = png.shape[0] * w2 // png.shape[1]
         mask = cv2.resize(png, (w2, h2))
 
-        ny = y - (h2 - h) // 2
+        y1 = y - (h2 - h) // 2
+        y2 = y1 + h2
+        if y1 < 0 or y2 >= frame.shape[0] or x + w2 >= frame.shape[1]:
+            continue
+
         alpha = mask[:, :, 3] / 255
         for i in range(3):
-            frame[ny:ny + h2, x:x + w2, i] = frame[ny:ny + h2, x:x + w2, i] * (1 - alpha) + mask[:, :, i] * alpha
+            frame[y1:y2, x:x + w2, i] = frame[y1:y2, x:x + w2, i] * (1 - alpha) + mask[:, :, i] * alpha
 
     cv2.imshow("frame", frame)
     key_code = cv2.waitKey(1)
