c0d2c911151f0df8370eeb67181ebebf4fc38fa7
Kinda Lam <lamkinun@gmail.com>
2019-12-31 17:13:09 +0800

    mouse event sample.

--- 016_MouseEvent.py
+++ 016_MouseEvent.py
@@ -2,27 +2,47 @@ import cv2
 
 
 def on_mouse_my_window(event, x, y, flags, params):
-    if event == cv2.EVENT_MOUSEMOVE:
-        print("Mouse Move:", x, y)
-    elif event == cv2.EVENT_LBUTTONDOWN:
+    global mode, images, btn_w
+
+    if event == cv2.EVENT_LBUTTONDOWN:
         print("L DOWN:", x, y, flags)
-    elif event == cv2.EVENT_LBUTTONUP:
-        print("L UP:", x, y, flags)
-    elif event == cv2.EVENT_LBUTTONDBLCLK:
-        print("L DB Click:", x, y, flags)
-    else:
-        print(event, x, y, flags, params)
+
+        m = x // 100
+        if m >= len(images):
+            mode = 0
+        else:
+            btn_h = btn_w * images[m].shape[0] // images[m].shape[1]
+            if y >= btn_h:
+                mode = 0
+            else:
+                mode = m + 1
 
 
 window_name = "My Window"
 cv2.namedWindow(window_name)
 cv2.setMouseCallback(window_name, on_mouse_my_window)
 
+mode = 0
+btn_w = 100
+images = list()
+images.append(cv2.imread("./images/sample_01.jpg"))
+images.append(cv2.imread("./images/sample_02.jpg"))
+
 camera = cv2.VideoCapture(0)
 while camera.isOpened():
     success, frame = camera.read()
     if not success:
         continue
+
+    if mode > 0:
+        frame = images[mode - 1]
+
+    for i, image in enumerate(images):
+        w = btn_w
+        h = w * image.shape[0] // image.shape[1]
+        img = cv2.resize(image, (w, h))
+        o = w * i
+        frame[:h, o:o+w, :] = img
 
     cv2.imshow(window_name, frame)
 
