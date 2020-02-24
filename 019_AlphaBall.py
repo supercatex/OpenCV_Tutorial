a448f01962832a75e8e7521e394a93cffe7de956
Kinda Lam <lamkinun@gmail.com>
2020-02-24 11:40:31 +0800

    AlphaBall2

--- 019_AlphaBall.py
+++ 019_AlphaBall.py
@@ -29,20 +29,19 @@ def join_images(img, alpha, bg, b):
     x2 = b["center"][0] + b["r"]
 
     if y1 < 0 or x1 < 0 or y2 >= image_h or x2 >= image_w:
-        cv2.waitKey(0)
+        left = max(0 - x1, 0)
+        right = max(x2 - image_w, 0)
+        top = max(0 - y1, 0)
+        bottom = max(y2 - image_h, 0)
 
-    # right = max(x2 - image_w, 0)
-    # left = max(0 - x1, 0)
-    # top = max(0 - y1, 0)
-    # bottom = max(y2 - image_h, 0)
-    # y1 += top
-    # y2 -= bottom
-    # x1 += left
-    # x2 -= right
-    #
-    # print(y1, y2, x1, x2)
-    # print(top, bottom, left, right)
-    # print(bg[y1:y2, x1:x2, :].shape, img[top:img.shape[0]-bottom, left:img.shape[1]-right, :].shape)
+        y1 += top
+        y2 -= bottom
+        x1 += left
+        x2 -= right
+
+        h, w = img.shape[:2]
+        img = img[top:h - bottom, left:w - right]
+        alpha = alpha[top:h - bottom, left:w - right]
 
     for i in range(3):
         bg[y1:y2, x1:x2, i] = bg[y1:y2, x1:x2, i] * (1 - alpha) + img[:, :, i] * alpha
@@ -51,25 +50,29 @@ def join_images(img, alpha, bg, b):
 
 image_w, image_h = (640, 480)
 
-ball_1 = create_ball(1, (image_w // 2, image_h // 2), (0, 0, 1), 1)
+ball_list = []
 
 while True:
-    image = np.zeros((image_h, image_w, 3))
-    image[:, :, :] = 1
-
-    ball_1_img, ball_1_alpha = create_ball_image(ball_1)
-    image = join_images(ball_1_img, ball_1_alpha, image, ball_1)
-
-    if ball_1["alpha"] > 0:
-        ball_1["alpha"] -= 0.01
-        ball_1["r"] += 0
-    else:
-        rx = np.random.randint(30, image_w - 30)
-        ry = np.random.randint(30, image_h - 30)
+    if np.random.randint(0, 10) == 0:
+        rx = np.random.randint(0, image_w)
+        ry = np.random.randint(0, image_h)
         rcr = np.random.random()
         rcg = np.random.random()
         rcb = np.random.random()
-        ball_1 = create_ball(30, (rx, ry), (rcb, rcg, rcr), 1)
+        new_ball = create_ball(1, (rx, ry), (rcb, rcg, rcr), 1)
+        ball_list.append(new_ball)
+    image = np.zeros((image_h, image_w, 3))
+    image[:, :, :] = 1
+
+    for b in ball_list:
+        b_img, b_alpha = create_ball_image(b)
+        image = join_images(b_img, b_alpha, image, b)
+
+        if b["alpha"] > 0:
+            b["alpha"] -= 0.01
+            b["r"] += 1
+        else:
+            ball_list.remove(b)
 
     cv2.imshow("image", image)
     key_code = cv2.waitKey(1)
